789a17264f11b48147b89b54b945c252093b6daa
Paul d'Aoust <amillionlobsters@gmail.com>
2024-06-22 05:35:13 -0700

    use scalars for arduinoMillisClock too

--- src/sources/arduinoMillisClock.hpp
+++ src/sources/arduinoMillisClock.hpp
@@ -11,17 +11,12 @@ using arduino_ms = au::Quantity<au::Milli<au::Seconds>, unsigned long>;
 using arduino_ms_float = au::Quantity<au::Milli<au::Seconds>, float>;
 constexpr auto make_arduino_ms = au::milli(au::seconds);
 
-
-source_fn<arduino_ms> arduinoMillisClock() {
-  return [](push_fn<arduino_ms> push) {
+source_fn<unsigned long> arduinoMillisClock() {
+  return [](push_fn<unsigned long> push) {
     return [push]() {
-      push(make_arduino_ms(millis()));
+      push(millis());
     };
   };
-}
-
-source_fn<arduino_ms_float> arduinoMillisClockFloat() {
-  return map_<arduino_ms, arduino_ms_float>(arduinoMillisClock(), [](arduino_ms value) { return au::rep_cast<float>(value); });
 }
 
 #endif
