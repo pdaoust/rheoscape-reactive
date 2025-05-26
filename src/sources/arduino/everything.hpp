f3d0c40f98befb43f1044fc83fc4e443523ca7ee
Paul d'Aoust <amillionlobsters@gmail.com>
2025-05-26 06:14:12 -0700

    feat: Arduino interrupt-based pin source

--- src/sources/arduino/everything.hpp
+++ src/sources/arduino/everything.hpp
@@ -1,3 +1,4 @@
 #include <sources/arduino/analogPinSource.hpp>
 #include <sources/arduino/bh1750.hpp>
 #include <sources/arduino/digitalPinSource.hpp>
+#include <sources/arduino/pinInterruptSource.hpp>
