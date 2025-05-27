6e210906726aa92d864c439b6e2ce1c643470efc
Paul d'Aoust <amillionlobsters@gmail.com>
2025-05-26 20:32:03 -0700

    edit: moved serial sinks around

---
+++ src/sinks/arduino/serialSinks.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <functional>
+#include <core_types.hpp>
+#include <Arduino.h>
+
+namespace rheo {
+
+  pullable_sink_fn<std::string> serialStringSink() {
+    return [](source_fn<std::string> source) {
+      return source(
+        [](std::string value) {
+          Serial.print(value.c_str());
+        },
+        [](){}
+      );
+    };
+  }
+
+  pullable_sink_fn<std::string> serialStringLineSink() {
+    return [](source_fn<std::string> source) {
+      return source(
+        [](std::string value) {
+          Serial.println(value.c_str());
+        },
+        [](){}
+      );
+    };
+  }
+
+}
