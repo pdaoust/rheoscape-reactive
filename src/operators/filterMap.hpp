dcfd7cce904b3032b34640c5dbd02a8510569b0a
Paul d'Aoust <amillionlobsters@gmail.com>
2025-06-23 18:36:44 -0700

    filterMap takes new filter_map_fn

--- src/operators/filterMap.hpp
+++ src/operators/filterMap.hpp
@@ -6,7 +6,7 @@
 namespace rheo::operators {
   
   template <typename TOut, typename TIn>
-  source_fn<TOut> filterMap(source_fn<TIn> source, map_fn<std::optional<TOut>, TIn> filterMapper) {
+  source_fn<TOut> filterMap(source_fn<TIn> source, filter_map_fn<TOut, TIn> filterMapper) {
     return [source, filterMapper](push_fn<TOut> push, end_fn end) {
       return source(
         [filterMapper, push](TIn value) {
@@ -21,7 +21,7 @@ namespace rheo::operators {
   }
 
   template <typename TOut, typename TIn>
-  pipe_fn<TOut, TIn> filterMap(map_fn<std::optional<TOut>, TIn> filterMapper) {
+  pipe_fn<TOut, TIn> filterMap(filter_map_fn<TOut, TIn> filterMapper) {
     return [filterMapper](source_fn<TIn> source) {
       return filterMap(source, filterMapper);
     };
