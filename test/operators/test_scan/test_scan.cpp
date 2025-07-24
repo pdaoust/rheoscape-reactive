b218c9325c04949f8d7d4c05d626eb7cd9b3a487
Paul d'Aoust <amillionlobsters@gmail.com>
2025-07-23 17:05:57 -0700

    update tests to use scan

--- test/operators/test_scan/test_scan.cpp
+++ test/operators/test_scan/test_scan.cpp
@@ -4,9 +4,9 @@
 #include <sources/sequence.hpp>
 #include <sources/empty.hpp>
 
-void test_scan_scans() {
-  auto someNumbers = rheo::sequence(0, 9, 1);
-  auto scanner = rheo::scan<std::string>(
+void test_scan_scans_with_initial() {
+  auto someNumbers = rheo::sources::sequence(0, 9, 1);
+  auto scanner = rheo::operators::scan<std::string, int>(
     someNumbers,
     "",
     (rheo::fold_fn<std::string, int>)[](std::string acc, int v) {
@@ -37,8 +37,41 @@ void test_scan_scans() {
   TEST_ASSERT_EQUAL_STRING_MESSAGE("0123456789", pushedValue.c_str(), "should scan acc with tenth item");
 }
 
+void test_scan_scans_without_initial() {
+  auto someNumbers = rheo::sources::sequence(0, 9, 1);
+  auto scanner = rheo::operators::scan<int>(
+    someNumbers,
+    (rheo::reduce_fn<int>)[](int acc, int v) {
+      return acc + v;
+    }
+  );
+  int pushedValue;
+  rheo::pull_fn pull = scanr([&pushedValue](int v) { pushedValue = v; }, [](){});
+  pull();
+  TEST_ASSERT_EQUAL_MESSAGE(0, pushedValue, "should yield first item");
+  pull();
+  TEST_ASSERT_EQUAL_MESSAGE(1, pushedValue, "should scan first item with second item");
+  pull();
+  TEST_ASSERT_EQUAL_MESSAGE(3, pushedValue, "should scan acc with third item");
+  pull();
+  TEST_ASSERT_EQUAL_MESSAGE(6, pushedValue, "should scan acc with fourth item");
+  pull();
+  TEST_ASSERT_EQUAL_MESSAGE(10, pushedValue, "should scan acc with fifth item");
+  pull();
+  TEST_ASSERT_EQUAL_MESSAGE(15, pushedValue, "should scan acc with sixth item");
+  pull();
+  TEST_ASSERT_EQUAL_MESSAGE(21, pushedValue, "should scan acc with seventh item");
+  pull();
+  TEST_ASSERT_EQUAL_MESSAGE(28, pushedValue, "should scan acc with eighth item");
+  pull();
+  TEST_ASSERT_EQUAL_MESSAGE(36, pushedValue, "should scan acc with ninth item");
+  pull();
+  TEST_ASSERT_EQUAL_MESSAGE(45, pushedValue, "should scan acc with tenth item");
+}
+
 int main(int argc, char **argv) {
   UNITY_BEGIN();
-  RUN_TEST(test_scan_scans);
+  RUN_TEST(test_scan_scans_with_initial);
+  RUN_TEST(test_scan_scans_without_initial);
   UNITY_END();
 }
