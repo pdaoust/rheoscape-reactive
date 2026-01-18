6f4d8cf8ec94d76347ac7e2499a67d0d411d82bc
Paul d'Aoust <paul.daoust@holo.host>
2026-01-17 16:33:11 -0800

    feat: servo sink

--- src/sinks/arduino/everything.hpp
+++ src/sinks/arduino/everything.hpp
@@ -2,3 +2,4 @@
 #include <sinks/arduino/analog_pin_sink.hpp>
 #include <sinks/arduino/digital_pin_sink.hpp>
 #include <sinks/arduino/serial_sinks.hpp>
+#include <sinks/arduino/servo_sink.hpp>
---
+++ src/sinks/arduino/servo_sink.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <functional>
+#include <core_types.hpp>
+#include <types/au_all_units_noio.hpp>
+#include <Arduino.h>
+#ifdef ESP32
+  #include <ESP32Servo.h>
+#else
+  #include <Servo.h>
+#endif
+
+
+namespace rheo::sinks::arduino {
+
+  struct servo_sink_push_handler {
+    std::shared_ptr<Servo> servo;
+    int pin;
+
+    RHEO_NOINLINE void operator()(au::QuantityPoint<au::Degrees, uint8_t> value) const {
+        servo->write(static_cast<int>(value.in(au::Degrees{})));
+    }
+  };
+
+  struct servo_sink_binder {
+    int pin;
+
+    RHEO_NOINLINE pull_fn operator()(source_fn<au::QuantityPoint<au::Degrees, uint8_t>> source) const {
+      auto servo = std::make_shared<Servo>();
+      servo->attach(pin);
+      return source(servo_sink_push_handler{std::move(servo), pin});
+    }
+  };
+
+  pullable_sink_fn<au::QuantityPoint<au::Degrees, uint8_t>> servo_sink(int pin) {
+    return servo_sink_binder{pin};
+  }
+
+}
