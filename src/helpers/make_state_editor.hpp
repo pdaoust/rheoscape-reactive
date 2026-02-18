52194e577ab5d9a5fdf491e60b45d4f722a7b4ea
Paul d'Aoust <paul.daoust@holo.host>
2026-02-18 11:57:51 -0800

    chore: docco on make_state_editor

--- src/helpers/make_state_editor.hpp
+++ src/helpers/make_state_editor.hpp
@@ -10,6 +10,13 @@ using namespace rheo::operators;
 
 namespace rheo::helpers {
 
+  // Construct a pipeline that binds a State<T> to a stream consisting of input events
+  // (for instance, button presses and rotary encoder clicks).
+  // The mapper function that you supply should take an input event as its first argument
+  // and the current value of the state as its argument,
+  // and transform the value of the state to a new value based on the input event.
+  // The returned pull function should be pulled regularly,
+  // unless the input event source pushes its values without being asked.
   template <typename T, typename InputSource, typename MapFn>
     requires concepts::Source<InputSource>
       && concepts::Transformer<MapFn, std::tuple<source_value_t<InputSource>, T>>
