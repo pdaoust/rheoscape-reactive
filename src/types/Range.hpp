5008723560ef2da17a27072961b7c0ed5b09a72a
Paul d'Aoust <paul.daoust@holo.host>
2026-02-26 06:00:14 -0800

    fix: range's properties aren't const

--- src/types/Range.hpp
+++ src/types/Range.hpp
@@ -5,8 +5,8 @@ namespace rheoscape {
   template <typename T>
   struct Range {
     using value_type = T;
-    const T min;
-    const T max;
+    T min;
+    T max;
 
     Range(const T min, const T max) : min(min), max(max) { }
   };
