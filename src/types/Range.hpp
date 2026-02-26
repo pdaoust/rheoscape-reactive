2e06d118abc09ec43a42aadf625b1476fa784936
Paul d'Aoust <paul.daoust@holo.host>
2026-02-26 06:00:34 -0800

    feat: comparison/containment/overlap operators for Range

--- src/types/Range.hpp
+++ src/types/Range.hpp
@@ -9,6 +9,28 @@ namespace rheoscape {
     T max;
 
     Range(const T min, const T max) : min(min), max(max) { }
+
+    bool operator==(Range<T> other) {
+      return min == other.min && max == other.max;
+    }
+
+    bool contains(T value) {
+      if (min == max) {
+        return value == min;
+      }
+      if (min < max) {
+        return min <= value && value <= max;
+      }
+      return min >= value && value >= max;
+    }
+
+    bool contains(Range<T> value) {
+      return contains(value.min) && contains(value.max);
+    }
+
+    bool overlaps(Range<T> value) {
+      return contains(value.min) || contains(value.max);
+    }
   };
 
 }
